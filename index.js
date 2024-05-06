c90aabc2db8ffe3512da974b6c95217d6744cab3
Pichayut Sombat <psb.nnt@gmail.com>
2024-05-07 06:54:46 +0700

    minor adj

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 
+
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
