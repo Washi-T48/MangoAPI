f5f9a16188449e86d09d8954a0dbfdb3468e88bb
Pichayut Sombat <psb.nnt@gmail.com>
2024-05-07 04:06:07 +0700

    Init

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+    console.log(req.body);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
